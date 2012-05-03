56e86f7dfe95cfe695cfd6b7b3367ab4db374154
Martin Wolters <sirdzstic@gmail.com>
2012-05-03 16:54:38 +0200

    Unicode is the way to go.

--- testdb.sh
+++ testdb.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 TITLE="Peters Blag"
-HEAD="Wer sch&ouml;ne Verschw&ouml;rungslinks f&uuml;r mich hat: ab an sirdzstic (at) gmail.com!"
+HEAD="Wer schöne Verschwörungslinks für mich hat: ab an sirdzstic (at) gmail.com!"
 TAIL="Proudly made without PHP, Java, Perl, MySQL and Postgres<br><a href=impressum.html>Impressum</a>"
-DESC="Ein Testblog fuer happyblog"
+DESC="Ein Platz für die tiefgehenden Gedanken und Gefühle von Peter"
 URL="http://chelloveck.vellocet.net/cgi-bin/blag.cgi"
 
 bin/createdb -t "$TITLE" -e "$HEAD" -f "$TAIL" -o $1
