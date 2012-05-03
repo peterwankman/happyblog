cc2b8dafc6931ab9c781da1f1c59da71fa1be637
Martin Wolters <sirdzstic@gmail.com>
2012-05-03 16:36:20 +0200

    Fix

--- testdb.sh
+++ testdb.sh
@@ -3,7 +3,7 @@
 TITLE="Peters Blag"
 HEAD="Wer sch&ouml;ne Verschw&ouml;rungslinks f&uuml;r mich hat: ab an sirdzstic (at) gmail.com!"
 TAIL="Proudly made without PHP, Java, Perl, MySQL and Postgres<br><a href=impressum.html>Impressum</a>"
-DESC="Ein Testblog f&uuml;r happyblog"
+DESC="Ein Testblog fuer happyblog"
 URL="http://chelloveck.vellocet.net/cgi-bin/blag.cgi"
 
 bin/createdb -t "$TITLE" -e "$HEAD" -f "$TAIL" -o $1
