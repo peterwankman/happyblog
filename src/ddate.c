4da5f1fad6093a3ff962198f3ae61256279ef7c6
Martin Wolters <sirdzstic@gmail.com>
2012-08-12 22:05:50 +0200

    Beautification

--- src/ddate.c
+++ src/ddate.c
@@ -1,17 +1,16 @@
 /* ddate.c .. converts boring normal dates to fun Discordian Date -><-
-   written  the 65th day of The Aftermath in the Year of Our Lady of 
+   written the 65th day of The Aftermath in the Year of Our Lady of 
    Discord 3157 by Druel the Chaotic aka Jeremy Johnson aka
-   mpython@gnu.ai.mit.edu  
+   mpython@gnu.ai.mit.edu	
 
    and I'm not responsible if this program messes anything up (except your 
    mind, I'm responsible for that)
 
    Modifications for Unix by Lee Harvey Oswald Smith, K.S.C.
    Five tons of flax.
-   
+	 
    Modifications for happyblog by MrLoom, K.S.C.
-   Dash it! Soft tabs with a tab size of 2, really?
-   -- Order and Disorder.
+   Code Beautification by Peter, L.D.D.
 */
 
 #include <stdlib.h>
@@ -19,83 +18,83 @@
 #include <time.h>
 #include <string.h>
 
-
-struct disc_time
-{int season; /* 0-4 */
- int day; /* 0-72 */
- int yday; /* 0-365 */
- int year; /* 3066- */
+struct disc_time {
+	int season; /* 0-4 */
+	int day; /* 0-72 */
+	int yday; /* 0-365 */
+	int year; /* 3066- */
 };
 
+static char *ending(int num) {	
+	int temp;
+	char *funkychickens;
 
-static char *ending(int num)
-{  
- int temp;
- char *funkychickens;
- 
- funkychickens=(char *)malloc(sizeof(char)*3);
- 
-  temp=num%10; /* get 0-9 */  
-  switch (temp)
-  { case 1:
-      strcpy(funkychickens,"st");
-      break;
-    case 2:
-      strcpy(funkychickens,"nd");
-      break;
-    case 3:
-      strcpy(funkychickens,"rd");
-      break;
-    default:
-      strcpy(funkychickens,"th");
-    }
- return funkychickens;
+	funkychickens=(char *)malloc(sizeof(char)*3);
+
+	temp=num%10; /* get 0-9 */	
+	switch (temp) { 
+		case 1:
+			strcpy(funkychickens,"st");
+			break;
+		case 2:
+			strcpy(funkychickens,"nd");
+			break;
+		case 3:
+			strcpy(funkychickens,"rd");
+			break;
+		default:
+			strcpy(funkychickens,"th");
+	}
+
+	return funkychickens;
 }
 
-static struct disc_time convert(int nday, int nyear)
-{  struct disc_time funkychickens;
-   
-   funkychickens.year = nyear+3066;
-   funkychickens.day=nday;
-   funkychickens.season=0;
-   if ((funkychickens.year%4)==2)
-     {if (funkychickens.day==59)
-       funkychickens.day=-1;
-     else if (funkychickens.day >59)
-       funkychickens.day-=1;
-    }
-   funkychickens.yday=funkychickens.day;
-   while (funkychickens.day>=73)
-     { funkychickens.season++;
-       funkychickens.day-=73;
-     }
-   return funkychickens;
-  
- }
+static struct disc_time convert(int nday, int nyear) {
+	struct disc_time funkychickens;
+	 
+	funkychickens.year = nyear+3066;
+	funkychickens.day=nday;
+	funkychickens.season=0;
 
-void ddate(char *buffer, size_t maxsize, struct tm *eris)
-{ char *days[5] = { "Sweetmorn",
-                    "Boomtime",
-                    "Pungenday",
-                    "Prickle-Prickle",
-                    "Setting Orange"
-  };
-  char *seasons[5] = {"Chaos",
-                      "Discord",
-                      "Confusion",
-                      "Bureaucracy",
-                      "The Aftermath"
-  };
-  char *end = NULL;
-  struct disc_time tick;
-  tick=convert(eris->tm_yday, eris->tm_year);
-  end = ending(tick.day);
-  if (tick.day==-1) snprintf(buffer, maxsize, "St. Tib's Day in the YOLD %d",tick.year);
-  else
-    { tick.day++;
-      snprintf(buffer, maxsize, "%s, %d%s day of %s - YOLD %d", days[tick.yday%5], tick.day, end,seasons[tick.season], tick.year);
-    }
-  if(end)
-    free(end);
+	if ((funkychickens.year%4)==2) {
+		if (funkychickens.day==59)
+			funkychickens.day=-1;
+		else if (funkychickens.day >59)
+			funkychickens.day-=1;
+	}
+
+	funkychickens.yday=funkychickens.day;
+	while (funkychickens.day>=73) {
+		funkychickens.season++;
+		funkychickens.day-=73;
+	}
+	return funkychickens;	
 }
 
+void ddate(char *buffer, size_t maxsize, struct tm *eris) { 
+	char *days[5] = { "Sweetmorn",
+		"Boomtime",
+		"Pungenday",
+		"Prickle-Prickle",
+		"Setting Orange"
+	};
+	char *seasons[5] = {"Chaos",
+		"Discord",
+		"Confusion",
+		"Bureaucracy",
+		"The Aftermath"
+	};
+	char *end = NULL;
+	struct disc_time tick;
+
+	tick=convert(eris->tm_yday, eris->tm_year);
+	end = ending(tick.day);
+	if (tick.day==-1)
+		snprintf(buffer, maxsize, "St. Tib's Day in the YOLD %d",tick.year);
+	else { 
+		tick.day++;
+		snprintf(buffer, maxsize, "%s, %d%s day of %s - YOLD %d", days[tick.yday%5], tick.day, end,seasons[tick.season], tick.year);
+	}
+	if(end)
+		free(end);
+}
